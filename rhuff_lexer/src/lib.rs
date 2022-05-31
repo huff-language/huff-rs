03aad6ea3f1b53a0df832a92195ac2e07c836125
virtual <106599035+xuuz@users.noreply.github.com>
2022-05-31 20:43:42 +0100

    feat: lexer skeleton

---
+++ rhuff_lexer/src/lib.rs
@@ -0,0 +1,64 @@
+use std::str::Chars;
+use std::iter::Peekable;
+
+enum OpcodeType {
+    // Opcode list goes here.
+}
+
+enum TokenType {
+    Opcode(OpcodeType),
+    Whitespace,
+    // Expand with more types.
+}
+
+struct Span {
+    start: usize,
+    end: usize,
+}
+
+struct Token {
+    ttype: TokenType,
+    span: Span,
+}
+
+struct Lexer<'a> {
+    pos: usize,
+    chars: Peekable<Chars<'a>>,
+    eof: bool,
+}
+
+impl<'a> Lexer<'a> {
+    fn new(source: &'a str) -> Self {
+        Self {
+            chars: source.chars().peekable(),
+            pos: 0,
+            eof: false,
+        }
+    }
+
+    fn consume() -> Option<char> {
+        // Move the lexer along one character.
+        Some('g')
+    }
+
+    fn peek_n(&self, n: usize) -> Option<char> {
+        self.chars.clone().nth(n)
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    // We need to think about errors.
+    type Item = Result<Token, ()>;
+
+    // The bulk of the lexing logic can reside here.
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(Ok(Token {
+            ttype: TokenType::Whitespace,
+            span: Span {
+                start: 0,
+                end: 0,
+            }
+        }))
+    }
+
+}
