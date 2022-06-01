8d1870d7f9e1710ded9b681589a429bf51484773
virtual <106599035+xuuz@users.noreply.github.com>
2022-06-01 13:39:40 +0100

    initial parser defs and structure

--- huff_parse/src/lib.rs
+++ huff_parse/src/lib.rs
@@ -1,8 +1,41 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use huffr_utils::token::Token;
+
+type Literal = [u8; 32];
+
+struct MacroDefinition<'a> {
+    name: String,
+    arguments: Vec<String>,
+    statements: Vec<Statement<'a>>,
+    takes: usize,
+    returns: usize,
+}
+
+struct MacroInvocation<'a> {
+    macro_name: String,
+    args: Vec<&'a Literal>,
+}
+
+enum Statement<'a> {
+    Literal(Literal),
+    Opcode,
+    MacroInvocation(MacroInvocation<'a>),
+}
+
+struct Parser<'a> {
+    cursor: usize,
+    tokens: Vec<Token<'a>>,
+}
+
+impl Parser<'_> {
+    fn new<'a>(tokens: Vec<Token<'static>>) -> Self {
+        Self { cursor: 0, tokens }
+    }
+
+    fn peek(&self) -> &Token {
+        self.tokens.get(self.cursor).unwrap()
+    }
+
+    fn peek_n(&self, n: usize) -> &Token {
+        self.tokens.get(self.cursor + n).unwrap()
     }
 }
