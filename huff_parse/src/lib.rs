f4112345ebe440b83ab018cded6d62b37e22a6d1
virtual <106599035+xuuz@users.noreply.github.com>
2022-06-01 14:43:57 +0100

    ast def

--- huff_parse/src/lib.rs
+++ huff_parse/src/lib.rs
@@ -1,26 +1,5 @@
 use huffr_utils::token::Token;
 
-type Literal = [u8; 32];
-
-struct MacroDefinition<'a> {
-    name: String,
-    arguments: Vec<String>,
-    statements: Vec<Statement<'a>>,
-    takes: usize,
-    returns: usize,
-}
-
-struct MacroInvocation<'a> {
-    macro_name: String,
-    args: Vec<&'a Literal>,
-}
-
-enum Statement<'a> {
-    Literal(Literal),
-    Opcode,
-    MacroInvocation(MacroInvocation<'a>),
-}
-
 struct Parser<'a> {
     cursor: usize,
     tokens: Vec<Token<'a>>,
