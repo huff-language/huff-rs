1442e58efd8c334093caded7aa183738e4ce702a
Andreas Bigger <abigger87@gmail.com>
2022-06-05 13:37:35 -0400

    clean docs

--- huff_parser/src/lib.rs
+++ huff_parser/src/lib.rs
@@ -9,6 +9,7 @@
 //!
 //! It also exposes a number of practical methods for accessing information about the source code
 //! throughout lexing.
+//!
 //! #### Usage
 //!
 //! The following example steps through the lexing of a simple, single-line source code macro
@@ -18,6 +19,22 @@
 //! use huff_utils::{token::*, span::*};
 //! use huff_lexer::{Lexer};
 //! use huff_parser::{Parser};
+//!
+//! // Mock source code as a string
+//! let source = "#define macro HELLO_WORLD() = takes(0) returns(0) {}";
+//!
+//! // Create a lexer from the source code
+//! let lexer = Lexer::new(source);
+//!
+//! // Grab the tokens from the lexer
+//! let tokens = lexer.into_iter().map(|x| x.unwrap()).collect::<Vec<Token>>();
+//!
+//! // Parser incantation
+//! let mut parser = Parser::new(tokens);
+//!
+//! // Parse into an AST
+//! parser.parse();
+//! assert_eq!(parser.current_token.kind, TokenKind::Eof);
 //! ```
 
 #![warn(missing_docs)]
