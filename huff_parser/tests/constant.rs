832974d705b10576348623381a18eec672444a8c
Ben Clabby <benclabby@gmail.com>
2022-06-06 14:31:57 -0400

    Don't include `0x` in `Literal`s when lexing

--- huff_parser/tests/constant.rs
+++ huff_parser/tests/constant.rs
@@ -36,7 +36,7 @@ fn parses_literal_constant() {
     // Create const val
     let mut arr: [u8; 32] = Default::default();
     let mut buf =
-        BytesMut::from("0x8C5BE1E5EBEC7D5BD14F71427D1E84F3DD0314C0F7B2291E5B200AC8C7C3B925");
+        BytesMut::from("8C5BE1E5EBEC7D5BD14F71427D1E84F3DD0314C0F7B2291E5B200AC8C7C3B925");
     buf.resize(32, 0);
     arr.copy_from_slice(buf.as_ref());
 
