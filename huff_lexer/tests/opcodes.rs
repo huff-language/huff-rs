c6408518033d4628c56f54d33e926ec856876e39
theNvN <thenvnsahu@gmail.com>
2022-06-16 23:18:30 +0530

    test: opcode inside scope

--- huff_lexer/tests/opcodes.rs
+++ huff_lexer/tests/opcodes.rs
@@ -1,26 +1,26 @@
 /// Tests lexing the Opcodes
 use huff_lexer::*;
-use huff_utils::{
-    evm::{OPCODES, OPCODES_MAP},
-    prelude::*,
-};
+use huff_utils::{evm::OPCODES, prelude::*};
 
 #[test]
 fn single_opcode() {
-    let source = "push1";
+    let source = "address { address }";
     let mut lexer = Lexer::new(source);
     assert_eq!(lexer.source, source);
 
-    // The first token should be opcode
+    // First token is `address` as Identifier (out of a scope)
     let tok = lexer.next().unwrap();
-    assert_eq!(
-        tok,
-        Token::new(
-            TokenKind::Opcode(OPCODES_MAP.get("push1").unwrap().to_owned()),
-            Span::new(0..source.len())
-        )
-    );
-    assert_eq!(lexer.span, Span::new(0..source.len()));
+    assert_eq!(tok, Token::new(TokenKind::Ident("address"), Span::new(0..7)));
+
+    // Second token is Open Brace
+    let _ = lexer.next().unwrap();
+
+    // The third token should be opcode `address` (inside a scope)
+    let tok = lexer.next().unwrap();
+    assert_eq!(tok, Token::new(TokenKind::Opcode("address"), Span::new(10..17)));
+
+    // Last token is Closing Brace
+    let _ = lexer.next().unwrap();
 
     // We should have reached EOF now
     assert_eq!(lexer.span.end, source.len());
@@ -30,21 +30,21 @@ fn single_opcode() {
 fn opcodes() {
     for opcode in OPCODES {
         let opcode = (*opcode).to_owned();
-        let source = opcode.clone();
+        // Opcode inside a scope
+        let source = format!("{{ {} }}", opcode); // { opcode_name }
+
         let mut lexer = Lexer::new(&source);
         assert_eq!(lexer.source, source);
 
-        // The first token should be opcode
+        // First token is Opening Brace
+        let _ = lexer.next().unwrap();
+        // The second token should be opcode
         let tok = lexer.next().unwrap();
-        assert_eq!(
-            tok,
-            Token::new(
-                TokenKind::Opcode(OPCODES_MAP.get(&opcode).unwrap().to_owned()),
-                Span::new(0..opcode.len())
-            )
-        );
-        assert_eq!(lexer.span, Span::new(0..opcode.len()));
+        assert_eq!(tok, Token::new(TokenKind::Opcode(&opcode), Span::new(2..2 + opcode.len())));
 
+        // Last token is Closing Brace
+        assert_eq!(lexer.span, Span::new(2..2 + opcode.len()));
+        let _ = lexer.next().unwrap();
         // We should have reached EOF now
         assert_eq!(lexer.span.end, source.len());
         assert!(lexer.next().is_none());
