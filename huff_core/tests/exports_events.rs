222a7ce3c9bb1e274f17b6e89c1be2771cade633
Andreas Bigger <abigger87@gmail.com>
2022-06-22 18:50:00 -0400

    add even tests

--- huff_core/tests/exports_events.rs
+++ huff_core/tests/exports_events.rs
@@ -147,7 +147,178 @@ fn test_abi_int_events() {
     );
 }
 
-// TODO::::
-// #define event SimpleEvent(address,address,uint256,string,uint)
-// #define event TupleArrays(address[],(address,uint256),(uint256,bool)[])
-// #define event NestedTupleArrays((address,(bool,bytes))[][],bytes,bytes32,string)
+#[test]
+fn test_abi_simple_events() {
+    let source: &str = r#"
+        #define event SimpleEvent(address,address,uint256,string,uint)
+        #define macro CONSTRUCTOR() = takes(0) returns (0) {}
+        #define macro MAIN() = takes(0) returns (0) {
+            0x00 calldataload 0xe0 shr
+        }
+    "#;
+
+    // Lex + Parse
+    let flattened_source = FullFileSource { source, file: None, spans: vec![] };
+    let lexer = Lexer::new(flattened_source);
+    let tokens = lexer.into_iter().map(|x| x.unwrap()).collect::<Vec<Token>>();
+    let mut parser = Parser::new(tokens, None);
+    let mut contract = parser.parse().unwrap();
+    contract.derive_storage_pointers();
+
+    let abi: Abi = contract.into();
+    assert_eq!(
+        abi.events,
+        BTreeMap::from([(
+            "SimpleEvent".to_string(),
+            huff_utils::abi::Event {
+                name: "SimpleEvent".to_string(),
+                inputs: vec![
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::Address,
+                        indexed: false
+                    },
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::Address,
+                        indexed: false
+                    },
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::Uint(256),
+                        indexed: false
+                    },
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::String,
+                        indexed: false
+                    },
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::Uint(256),
+                        indexed: false
+                    }
+                ],
+                anonymous: false
+            }
+        )])
+    );
+}
+
+#[ignore]
+#[test]
+fn test_abi_tuple_array_events() {
+    let source: &str = r#"
+        #define event TupleArrays(address[],(address,uint256),(uint256,bool)[])
+        #define macro CONSTRUCTOR() = takes(0) returns (0) {}
+        #define macro MAIN() = takes(0) returns (0) {
+            0x00 calldataload 0xe0 shr
+        }
+    "#;
+
+    // Lex + Parse
+    let flattened_source = FullFileSource { source, file: None, spans: vec![] };
+    let lexer = Lexer::new(flattened_source);
+    let tokens = lexer.into_iter().map(|x| x.unwrap()).collect::<Vec<Token>>();
+    let mut parser = Parser::new(tokens, None);
+    let mut contract = parser.parse().unwrap();
+    contract.derive_storage_pointers();
+
+    let abi: Abi = contract.into();
+    assert_eq!(
+        abi.events,
+        BTreeMap::from([(
+            "SimpleEvent".to_string(),
+            huff_utils::abi::Event {
+                name: "SimpleEvent".to_string(),
+                inputs: vec![
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::Address,
+                        indexed: false
+                    },
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::Address,
+                        indexed: false
+                    },
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::Uint(256),
+                        indexed: false
+                    },
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::String,
+                        indexed: false
+                    },
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::Uint(256),
+                        indexed: false
+                    }
+                ],
+                anonymous: false
+            }
+        )])
+    );
+}
+
+#[ignore]
+#[test]
+fn test_abi_nested_tuple_array_events() {
+    let source: &str = r#"
+        #define event NestedTupleArrays((address,(bool,bytes))[][],bytes,bytes32,string)
+        #define macro CONSTRUCTOR() = takes(0) returns (0) {}
+        #define macro MAIN() = takes(0) returns (0) {
+            0x00 calldataload 0xe0 shr
+        }
+    "#;
+
+    // Lex + Parse
+    let flattened_source = FullFileSource { source, file: None, spans: vec![] };
+    let lexer = Lexer::new(flattened_source);
+    let tokens = lexer.into_iter().map(|x| x.unwrap()).collect::<Vec<Token>>();
+    let mut parser = Parser::new(tokens, None);
+    let mut contract = parser.parse().unwrap();
+    contract.derive_storage_pointers();
+
+    let abi: Abi = contract.into();
+    assert_eq!(
+        abi.events,
+        BTreeMap::from([(
+            "SimpleEvent".to_string(),
+            huff_utils::abi::Event {
+                name: "SimpleEvent".to_string(),
+                inputs: vec![
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::Address,
+                        indexed: false
+                    },
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::Address,
+                        indexed: false
+                    },
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::Uint(256),
+                        indexed: false
+                    },
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::String,
+                        indexed: false
+                    },
+                    EventParam {
+                        name: "".to_string(),
+                        kind: FunctionParamType::Uint(256),
+                        indexed: false
+                    }
+                ],
+                anonymous: false
+            }
+        )])
+    );
+}
