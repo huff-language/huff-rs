61f9a4cfbc3ae1c62c5ea7210caadc24f9f5a336
exp_table <exp.table@protonmail.com>
2022-06-25 13:22:17 +0200

    better test

--- huff_parser/tests/opcodes.rs
+++ huff_parser/tests/opcodes.rs
@@ -4,24 +4,33 @@ use huff_utils::{evm::Opcode, prelude::*};
 
 #[test]
 fn not_mistaken_as_opcode() {
-    let source = r#"
-        #define macro IS_AUTHORIZED(not_authed_arg) = takes(0) returns(0) {}
-        #define macro MAIN() = takes(0) returns(0) {
-            IS_AUTHORIZED(not_authed)
-            not_authed:
-                return
-        }
-    "#;
-    let flattened_source = FullFileSource { source, file: None, spans: vec![] };
-    let lexer = Lexer::new(flattened_source);
+    for opcode in OPCODES {
+        let label = format!("{}_label", (*opcode).to_owned());
+        let source = &format!(
+            r#"
+                #define macro IS_AUTHORIZED(some_arg) = takes(0) returns(0) {{}}
+                #define macro MAIN() = takes(0) returns(0) {{
+                    IS_AUTHORIZED({})
+                    {}:
+                        return
+                }}
+            "#,
+            label, label
+        );
+        let flattened_source = FullFileSource { source, file: None, spans: vec![] };
+        let lexer = Lexer::new(flattened_source);
 
-    let tokens = lexer
-        .into_iter()
-        .map(|x| x.unwrap())
-        .filter(|x| !matches!(x.kind, TokenKind::Whitespace))
-        .collect::<Vec<Token>>();
-    let actual_label = tokens[tokens.len() - 5].kind.clone();
-    let mut parser = Parser::new(tokens, None);
-    let contract = parser.parse().unwrap();
-    assert_eq!(actual_label, TokenKind::Label("not_authed".to_string()));
+        let tokens = lexer
+            .into_iter()
+            .map(|x| x.unwrap())
+            .filter(|x| !matches!(x.kind, TokenKind::Whitespace))
+            .collect::<Vec<Token>>();
+        let actual_label_arg = tokens[tokens.len() - 7].kind.clone();
+        let actual_label = tokens[tokens.len() - 5].kind.clone();
+        let mut parser = Parser::new(tokens, None);
+        // parsing to ensure tokens syntax is valid
+        let contract = parser.parse().unwrap();
+        assert_eq!(actual_label_arg, TokenKind::Ident(label.clone()));
+        assert_eq!(actual_label, TokenKind::Label(label));
+    }
 }
