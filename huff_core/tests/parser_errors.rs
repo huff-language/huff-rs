441707452e75c1e79d32a0298503fc8073861324
clabby <ben@clab.by>
2022-06-30 21:11:42 -0400

    Don't pull random chars that are > 1 byte in `test_invalid_single_arg`

--- huff_core/tests/parser_errors.rs
+++ huff_core/tests/parser_errors.rs
@@ -250,7 +250,10 @@ fn test_invalid_token_in_label_definition() {
 fn test_invalid_single_arg() {
     for _ in [0..10_000] {
         let random_char = rand::random::<u8>() as char;
-        if random_char.is_numeric() || !random_char.is_alphabetic() {
+        if random_char.is_numeric() ||
+            !random_char.is_alphabetic() ||
+            random_char.to_string().as_bytes().len() > 1
+        {
             continue
         }
         let source =
