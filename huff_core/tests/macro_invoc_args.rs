57d929c649a18050042fb5ae3ba7e3cdc4a8cd7e
clabby <ben@clab.by>
2022-08-03 10:58:20 -0400

    Add bubbling tests for arg calls

--- huff_core/tests/macro_invoc_args.rs
+++ huff_core/tests/macro_invoc_args.rs
@@ -7,7 +7,7 @@ use std::str::FromStr;
 #[test]
 fn test_opcode_macro_args() {
     let source = r#"
-        #define macro RETURN1(zero) = takes(1) returns(0) {
+        #define macro RETURN1(zero) = takes(0) returns(0) {
             <zero> mstore
             0x20 <zero> return
         }
@@ -43,7 +43,7 @@ fn test_all_opcodes_in_macro_args() {
     for o in OPCODES {
         let source = format!(
             r#"
-            #define macro RETURN1(zero) = takes(1) returns(0) {{
+            #define macro RETURN1(zero) = takes(0) returns(0) {{
                 <zero>
             }}
 
@@ -81,12 +81,163 @@ fn test_constant_macro_arg() {
     let source = r#"
             #define constant A = 0x02
 
-            #define macro RETURN1(zero) = takes(1) returns(0) {
+            #define macro RETURN1(zero) = takes(0) returns(0) {
                 <zero>
             }
 
             #define macro MAIN() = takes(0) returns(0) {
                 RETURN1(A)
+            }
+        "#;
+
+    // Lex + Parse
+    let flattened_source = FullFileSource { source, file: None, spans: vec![] };
+    let lexer = Lexer::new(flattened_source);
+    let tokens = lexer.into_iter().map(|x| x.unwrap()).collect::<Vec<Token>>();
+    let mut parser = Parser::new(tokens, None);
+    let mut contract = parser.parse().unwrap();
+    contract.derive_storage_pointers();
+
+    // Create main and constructor bytecode
+    let main_bytecode = Codegen::generate_main_bytecode(&contract).unwrap();
+
+    // Full expected bytecode output (generated from huffc) (placed here as a reference)
+    let expected_bytecode = "60088060093d393df360ff6002";
+
+    // Create bytecode
+    let bytecode = format!("60088060093d393df360ff{}", main_bytecode);
+
+    // Check the bytecode
+    assert_eq!(bytecode.to_lowercase(), expected_bytecode.to_lowercase());
+}
+
+#[test]
+fn test_bubbled_label_call_macro_arg() {
+    let source = r#"
+            #define macro MACRO_A(zero) = takes(0) returns(0) {
+                MACRO_B(<zero>)
+            }
+
+            #define macro MACRO_B(zero) = takes(0) returns(0) {
+                <zero>
+            }
+
+            #define macro MAIN() = takes(0) returns(0) {
+                label:
+                    MACRO_A(label)
+            }
+        "#;
+
+    // Lex + Parse
+    let flattened_source = FullFileSource { source, file: None, spans: vec![] };
+    let lexer = Lexer::new(flattened_source);
+    let tokens = lexer.into_iter().map(|x| x.unwrap()).collect::<Vec<Token>>();
+    let mut parser = Parser::new(tokens, None);
+    let mut contract = parser.parse().unwrap();
+    contract.derive_storage_pointers();
+
+    // Create main and constructor bytecode
+    let main_bytecode = Codegen::generate_main_bytecode(&contract).unwrap();
+
+    // Full expected bytecode output (generated from huffc) (placed here as a reference)
+    let expected_bytecode = "60088060093d393df360ff5b610000";
+
+    // Create bytecode
+    let bytecode = format!("60088060093d393df360ff{}", main_bytecode);
+
+    // Check the bytecode
+    assert_eq!(bytecode.to_lowercase(), expected_bytecode.to_lowercase());
+}
+
+#[test]
+fn test_bubbled_literal_macro_arg() {
+    let source = r#"
+            #define macro MACRO_A(zero) = takes(0) returns(0) {
+                MACRO_B(<zero>)
+            }
+
+            #define macro MACRO_B(zero) = takes(0) returns(0) {
+                <zero>
+            }
+
+            #define macro MAIN() = takes(0) returns(0) {
+                MACRO_A(0x420)
+            }
+        "#;
+
+    // Lex + Parse
+    let flattened_source = FullFileSource { source, file: None, spans: vec![] };
+    let lexer = Lexer::new(flattened_source);
+    let tokens = lexer.into_iter().map(|x| x.unwrap()).collect::<Vec<Token>>();
+    let mut parser = Parser::new(tokens, None);
+    let mut contract = parser.parse().unwrap();
+    contract.derive_storage_pointers();
+
+    // Create main and constructor bytecode
+    let main_bytecode = Codegen::generate_main_bytecode(&contract).unwrap();
+
+    // Full expected bytecode output (generated from huffc) (placed here as a reference)
+    let expected_bytecode = "60088060093d393df360ff610420";
+
+    // Create bytecode
+    let bytecode = format!("60088060093d393df360ff{}", main_bytecode);
+
+    // Check the bytecode
+    assert_eq!(bytecode.to_lowercase(), expected_bytecode.to_lowercase());
+}
+
+#[test]
+fn test_bubbled_opcode_macro_arg() {
+    let source = r#"
+            #define macro MACRO_A(zero) = takes(0) returns(0) {
+                MACRO_B(<zero>)
+            }
+
+            #define macro MACRO_B(zero) = takes(0) returns(0) {
+                <zero>
+            }
+
+            #define macro MAIN() = takes(0) returns(0) {
+                MACRO_A(returndatasize)
+            }
+        "#;
+
+    // Lex + Parse
+    let flattened_source = FullFileSource { source, file: None, spans: vec![] };
+    let lexer = Lexer::new(flattened_source);
+    let tokens = lexer.into_iter().map(|x| x.unwrap()).collect::<Vec<Token>>();
+    let mut parser = Parser::new(tokens, None);
+    let mut contract = parser.parse().unwrap();
+    contract.derive_storage_pointers();
+
+    // Create main and constructor bytecode
+    let main_bytecode = Codegen::generate_main_bytecode(&contract).unwrap();
+
+    // Full expected bytecode output (generated from huffc) (placed here as a reference)
+    let expected_bytecode = "60088060093d393df360ff3d";
+
+    // Create bytecode
+    let bytecode = format!("60088060093d393df360ff{}", main_bytecode);
+
+    // Check the bytecode
+    assert_eq!(bytecode.to_lowercase(), expected_bytecode.to_lowercase());
+}
+
+#[test]
+fn test_bubbled_constant_macro_arg() {
+    let source = r#"
+            #define constant A = 0x02
+
+            #define macro MACRO_A(zero) = takes(0) returns(0) {
+                MACRO_B(<zero>)
+            }
+
+            #define macro MACRO_B(zero) = takes(0) returns(0) {
+                <zero>
+            }
+
+            #define macro MAIN() = takes(0) returns(0) {
+                MACRO_A(A)
             }
         "#;
 
