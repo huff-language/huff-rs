a410c9f2f21c89def4ba68bbebfe578aada1e0ed
Andreas Bigger <abigger87@gmail.com>
2022-08-16 22:05:54 -0400

    fix aliased import tests

--- huff_core/tests/aliased_imports.rs
+++ huff_core/tests/aliased_imports.rs
@@ -3,21 +3,30 @@ use std::{str::FromStr, sync::Arc};
 use huff_core::Compiler;
 use huff_utils::prelude::*;
 
-// TODO: fix this test by writing broken inputs to a temp file?
-// #[test]
-// fn test_parses_foundry_aliased_imports() {
-//     println!("In env: {:?}", std::env::current_dir().unwrap());
-//     std::env::set_current_dir("../");
-//     println!("In env: {:?}", std::env::current_dir().unwrap());
-//     let import_bufs =
-//         vec![std::path::PathBuf::from_str("examples/erc20/contracts/ERC20.huff").unwrap()];
-//     let potentials = Compiler::fetch_sources(import_bufs)
-//         .into_iter()
-//         .collect::<Vec<Result<Arc<FileSource>, CompilerError<'_>>>>();
-//     for import in potentials {
-//         import.unwrap();
-//     }
-// }
+#[test]
+fn test_parses_foundry_aliased_imports() {
+    // Set the current directory to the root of huff-rs
+    std::env::set_current_dir("../").unwrap();
+
+    // Create a remapper at the root level
+    let remapper = Remapper::new("./");
+
+    // Use an aliased import defined in foundry.toml for "huff-examples" -> "examples"
+    let mut import_bufs =
+        vec![std::path::PathBuf::from_str("examples/erc20/contracts/ERC20.huff").unwrap()];
+
+    // Remap import bufs with `remapper`. Panic on failure.
+    import_bufs = import_bufs
+        .into_iter()
+        .map(|p| std::path::PathBuf::from(remapper.remap(p.to_str().unwrap()).unwrap()))
+        .collect();
+
+    // Fetch sources and unwrap errors
+    let _ = Compiler::fetch_sources(import_bufs)
+        .into_iter()
+        .map(|r| r.unwrap())
+        .collect::<Vec<Arc<FileSource>>>();
+}
 
 #[test]
 #[should_panic]
