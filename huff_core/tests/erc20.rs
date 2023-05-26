859d869dd1621769780dc5ba06b9e5bfe5db3c87
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2023-05-26 00:07:57 +0000

    feat: add paris codegen tests

--- huff_core/tests/erc20.rs
+++ huff_core/tests/erc20.rs
@@ -34,26 +34,52 @@ fn test_erc20_compile() {
     let mut contract = parser.parse().unwrap();
     contract.derive_storage_pointers();
 
-    // Create main and constructor bytecode
-    let main_bytecode =
-        Codegen::generate_main_bytecode(&EVMVersion::default(), &contract, None).unwrap();
-    let (constructor_bytecode, has_custom_bootstrap) =
-        Codegen::generate_constructor_bytecode(&EVMVersion::default(), &contract, None).unwrap();
+    // Create main and constructor bytecode using the paris compatible evm version
+    let paris_evm = EVMVersion::new(SupportedEVMVersions::Paris);
+
+    let paris_main_bytecode = Codegen::generate_main_bytecode(&paris_evm, &contract, None).unwrap();
+    let (paris_constructor_bytecode, paris_has_custom_bootstrap) =
+        Codegen::generate_constructor_bytecode(&paris_evm, &contract, None).unwrap();
+
+    // Create main and constructor bytecode using the shanghai compatible evm version
+    let shanghai_evm = EVMVersion::new(SupportedEVMVersions::Shanghai);
+
+    let shanghai_main_bytecode =
+        Codegen::generate_main_bytecode(&shanghai_evm, &contract, None).unwrap();
+    let (shanghai_constructor_bytecode, has_custom_bootstrap) =
+        Codegen::generate_constructor_bytecode(&shanghai_evm, &contract, None).unwrap();
 
     // Churn
     let mut cg = Codegen::new();
-    let artifact = cg
+    let paris_artifact = cg
         .churn(
             Arc::clone(file_source),
             vec![],
-            &main_bytecode,
-            &constructor_bytecode,
-            has_custom_bootstrap,
+            &paris_main_bytecode,
+            &paris_constructor_bytecode,
+            paris_has_custom_bootstrap,
         )
         .unwrap();
 
     // Full expected bytecode output (generated from huffc)
-    let expected_bytecode = "335f5561019980600d3d393df35f3560e01c8063a9059cbb1461004757806340c10f19146100d757806370a082311461014157806318160ddd1461015c578063095ea7b314610166578063dd62ed3e1461017d575b600435336024358160016000526000602001526040600020548082116100d3578190038260016000526000602001526040600020558281906001600052600060200152604060002054018360016000526000602001526040600020555f527fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef60205fa360015f5260205ff35b5f5ffd5b5f5433146100e3575f5ffd5b6004355f60243582819060016000526000602001526040600020540183600160005260006020015260406000205580600254016002555f527fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef60205fa35b60043560016000526000602001526040600020545f5260205ff35b6002545f5260205ff35b602435600435336000526000602001526040600020555b6024356004356000526000602001526040600020545f5260205ff3";
+    let expected_paris_bytecode = "336000556101ac80600e3d393df360003560e01c8063a9059cbb1461004857806340c10f19146100de57806370a082311461014e57806318160ddd1461016b578063095ea7b314610177578063dd62ed3e1461018e575b600435336024358160016000526000602001526040600020548082116100d8578190038260016000526000602001526040600020558281906001600052600060200152604060002054018360016000526000602001526040600020556000527fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef60206000a3600160005260206000f35b60006000fd5b60005433146100ed5760006000fd5b600435600060243582819060016000526000602001526040600020540183600160005260006020015260406000205580600254016002556000527fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef60206000a35b600435600160005260006020015260406000205460005260206000f35b60025460005260206000f35b602435600435336000526000602001526040600020555b60243560043560005260006020015260406000205460005260206000f3";
 
-    assert_eq!(artifact.bytecode.to_lowercase(), expected_bytecode.to_lowercase());
+    assert_eq!(paris_artifact.bytecode.to_lowercase(), expected_paris_bytecode.to_lowercase());
+
+    let shanghai_artifact = cg
+        .churn(
+            Arc::clone(file_source),
+            vec![],
+            &shanghai_main_bytecode,
+            &shanghai_constructor_bytecode,
+            has_custom_bootstrap,
+        )
+        .unwrap();
+
+    let expected_shanghai_bytecode = "335f5561019980600d3d393df35f3560e01c8063a9059cbb1461004757806340c10f19146100d757806370a082311461014157806318160ddd1461015c578063095ea7b314610166578063dd62ed3e1461017d575b600435336024358160016000526000602001526040600020548082116100d3578190038260016000526000602001526040600020558281906001600052600060200152604060002054018360016000526000602001526040600020555f527fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef60205fa360015f5260205ff35b5f5ffd5b5f5433146100e3575f5ffd5b6004355f60243582819060016000526000602001526040600020540183600160005260006020015260406000205580600254016002555f527fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef60205fa35b60043560016000526000602001526040600020545f5260205ff35b6002545f5260205ff35b602435600435336000526000602001526040600020555b6024356004356000526000602001526040600020545f5260205ff3";
+
+    assert_eq!(
+        shanghai_artifact.bytecode.to_lowercase(),
+        expected_shanghai_bytecode.to_lowercase()
+    );
 }
